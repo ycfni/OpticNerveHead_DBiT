d38884fae118158f70dc317f678d16c5a135b439
Marcello DiStasio <marcello.distasio@yale.edu>
2023-01-22 00:03:21 -0500

    star ref

--- Analysis/Scripts/BuildSTARreference.sh
+++ Analysis/Scripts/BuildSTARreference.sh
@@ -16,3 +16,6 @@ OUTDIR=`pwd`/../../ReferenceFiles/hg38_STAR
 GENOMEDIR="/gpfs/ycga/datasets/genomes/Homo_sapiens/NCBI/GRCh38/"
 
 STAR --runThreadN 11 --runMode genomeGenerate --genomeDir $OUTDIR --genomeFastaFiles $GENOMEDIR/Sequence/WholeGenomeFasta/genome.fa --sjdbGTFfile $GENOMEDIR/Annotation/Genes/genes.gtf 
+
+# Link the annotations to a local folder to simplify the st_pipeline submission script
+ln -s /gpfs/ycga/datasets/genomes/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/genes.gtf `pwd`/../../ReferenceFiles/genes.gtf
