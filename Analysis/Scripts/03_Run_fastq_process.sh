26f659a0a41698f66edee0c85ee065bf42ab4e39
Marcello DiStasio <marcello.distasio@yale.edu>
2023-03-24 12:19:17 -0400

    new sample

--- Analysis/Scripts/03_Run_fastq_process.sh
+++ Analysis/Scripts/03_Run_fastq_process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=general
-#SBATCH --job-name=stpipeline
+#SBATCH --job-name=fastq_process
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=64G
 #SBATCH --time=72:00:00
@@ -19,6 +19,6 @@ conda activate stpipeline
 # NEED TO RUN OpticNerveHead_DBiT/Analysis/Python/fastq_process.py on *_[Read|R|]*2.fastq first
 # FASTQ reads
 
-FW_orig=`/../../data/raw/Undetermined_S0_L002_R2.fq.gz"
+FW_orig=`pwd`/../../data/A22-1834_slide3/raw/usftp21.novogene.com/01.RawData/A221834_ON2/A221834_ON2_CKDL230007519-1A_H3NY2DSX7_L2_2.fq.gz
 
-python ../Python/fastq_process.py $FW_orig
+python ../Python/fastq_process.py -i $FW_orig
--- Analysis/Scripts/04_Run_stpipeline.sh
+++ Analysis/Scripts/04_Run_stpipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=general
-#SBATCH --job-name=stpipeline_A221650_s2
+#SBATCH --job-name=stpipeline
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=64G
 #SBATCH --time=72:00:00
@@ -19,18 +19,18 @@ conda activate stpipeline
 
 # NEED TO RUN OpticNerveHead_DBiT/Analysis/Python/fastq_process.py on *_[Read|R|]*2.fastq first
 # FASTQ reads
-RV=`pwd`/../../data/A22-1650_slide2/raw/usftp21.novogene.com/01.RawData/A22_1650_ON2/A22_1650_ON2_CKDL230003511-1A_HT2J2DSX5_L1_1.fq.gz
-FW=`pwd`/../../data/A22-1650_slide2/raw/usftp21.novogene.com/01.RawData/A22_1650_ON2/A22_1650_ON2_CKDL230003511-1A_HT2J2DSX5_L1_2_processed.fastq.gz
+RV=`pwd`/../../data/A22-1834_slide3/raw/usftp21.novogene.com/01.RawData/A221834_ON2/A221834_ON2_CKDL230007519-1A_H3NY2DSX7_L2_1.fq.gz
+FW=`pwd`/../../data/A22-1834_slide3/raw/usftp21.novogene.com/01.RawData/A221834_ON2/A221834_ON2_CKDL230007519-1A_H3NY2DSX7_L2_2_processed.fastq.gz
 
 # References for mapping, annotation and nonRNA-filtering
 MAP=`pwd`/../../ReferenceFiles/hg38_STAR/
 ANN=`pwd`/../../ReferenceFiles/genes.gtf
 
 # Barcodes settings
-ID=`pwd`/../spatial_barcodes_index.txt 
+ID=`pwd`/../Library/spatial_barcodes_index_51_100.txt 
 
 # Output folder and experiment name
-PATH_TO_OUTPUT=`pwd`/../../data/A22-1650_slide2/processed/
+PATH_TO_OUTPUT=`pwd`/../../data/A22-1834_slide3/processed/
 OUTPUT=$PATH_TO_OUTPUT/st_pipeline_out/
 mkdir -p $OUTPUT
 
@@ -39,7 +39,7 @@ TMP=$PATH_TO_TEMP/st_pipeline_tmp/
 mkdir -p $TMP
 
 # EXP is the experiment name Do not add / or \ to the experiment name
-EXP=ONH_A22-1650_S2
+EXP=ONH_A22-1834_S3
 
 # Running the pipeline
 st_pipeline_run.py \
