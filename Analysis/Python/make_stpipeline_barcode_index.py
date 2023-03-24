c8285fd81ae705d4d7b027ad77ceefc3fee83320
Marcello DiStasio <mdistasio@localhost.localdomain>
2023-03-23 22:15:56 -0400

    Added make_stpipeline_barcode_index.py to create A & B barcode sequence with their respective locations for input to st_pipeline

---
+++ Analysis/Python/make_stpipeline_barcode_index.py
@@ -0,0 +1,76 @@
+import sys
+import argparse
+import itertools
+import openpyxl
+
+# Define command line argument parser
+parser = argparse.ArgumentParser(description='Convert xlsx file to tab-delimited text file.')
+parser.add_argument('input_file', metavar='input_file', type=str, help='Path to input xlsx file.')
+parser.add_argument('output_file', metavar='output_file', default="combinations.txt", type=str, help='Path to output txt file.')
+parser.add_argument('start_index', metavar='start_index', nargs='?', type=int, help='Which row to start with.')
+parser.add_argument('end_index', metavar='end_index', nargs='?', type=int, help='Which row to end with.')
+args = parser.parse_args()
+
+# Load input xlsx file
+workbook = openpyxl.load_workbook(args.input_file)
+worksheet = workbook.active
+
+# Get all values in the first column of the spreadsheet
+values = [cell.value for cell in worksheet['A']]
+
+if args.start_index:
+    starti = args.start_index
+else:
+    starti = 1
+if args.end_index:
+    endi = args.end_index
+else:
+    endi = len(values)
+if starti >= endi:
+    print("Ending index must be greater that starting index. Exiting.")
+    sys.exit()
+if (starti > len(values)) or (endi > len(values)):
+    print("Start and end indexes must not be greater than the number of rows in the input file. Exiting.")
+    sys.exit()
+
+print("Starting with row " + str(starti) + ".")
+print("Ending with row " + str(endi) + ".")
+
+valsel = values[starti-1:endi]
+
+# Generate all combinations of strings
+combinations = [''.join(x) for x in itertools.product(valsel, repeat=2)]
+
+index_combinations = ['\t'.join([str(i) for i in x]) for x in itertools.product(range(endi-starti+1, 1, -1), range(1, endi-starti+2), repeat=1)]
+
+output = ["{}\t{}".format(a_, b_) for a_, b_ in zip(combinations, index_combinations)]
+
+# Write combinations to a text file
+with open(args.output_file, 'w') as outfile:
+    outfile.write('\n'.join(output))
+
+
+
+    
+
+# # Open output text file
+# output_file = open("output.txt", "w")
+
+# # Loop through rows in worksheet
+# for row in worksheet.iter_rows(values_only=True):
+#     # Get string value in first column
+#     string_value = row[0]
+    
+#     # Get length of string value
+#     string_length = len(string_value)
+    
+#     # Loop through all combinations of two integer sequence indexes
+#     for i in range(string_length):
+#         for j in range(i+1, string_length+1):
+#             # Write combination to output text file
+#             output_file.write(string_value + '\t' + str(i) + '\t' + str(j) + '\n')
+
+# # Close output text file
+# output_file.close()
+
+# print("Conversion complete!")
